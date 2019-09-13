75cbf6fec31f9b93cd65a654cffb9ff5da04dbc9
Manlio Joaquín García González <>
2019-09-13 22:32:20 +0100

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+for dir in \
+    .ssh \
+    .local \
+    workbench \
+    forge \
+    vault \
+; do
+    if [ ! -d $HOME/$dir ]; then
+        mkdir -p $HOME/$dir
+    fi
+done
+
+if [ -d $HOME/Plantillas ]; then
+    rmdir $HOME/Plantillas
+    ln -s $HOME/.dotfiles/plantillas $HOME/Plantillas
+fi
+
+if [ ! -f $HOME/.ssh/config ]; then
+    scp -r -P 42 $USER@atanor.dev:.ssh/* $HOME/.ssh/.
+fi
+
+sudo apt install \
+    build-essential \
+    cowsay \
+    curl \
+    fortune \
+    git \
+    htop \
+    lynx \
+    tmux \
+    wget \
+-y
+
+if [ ! -d $HOME/.dotfiles ]; then
+    git clone "git@git.atanor.dev:quarkex/dotfiles.git" \
+        $HOME/.dotfiles --recurse-submodules
+fi
+
+sudo cp $HOME/.dotfiles/tmux.conf /etc/tmux.conf
+
+for file in \
+    vim \
+    vimrc \
+    lynxrc \
+    gitconfig \
+    Xdefaults \
+    Xresources \
+; do
+    if [ ! -e $HOME/".$file" ]; then
+        ln -s $HOME/.dotfiles/"$file" $HOME/".$file"
+    fi
+done
+
+if [ ! "$(tail -n 4 $HOME/.bashrc | head -n 1)" == "# Load personal bashrc" ];
+then echo '
+
+# Load personal bashrc
+if [ -f ~/.dotfiles/bashrc ]; then
+    . ~/.dotfiles/bashrc
+fi'>>$HOME/.bashrc
+fi
+
