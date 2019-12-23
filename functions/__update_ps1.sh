2115d909edd25b1403dd18ae777c06bd37f1b0db
Manlio Joaquín García González <quarkex@pm.me>
2019-12-23 12:23:10 +0000

    Move input prompt to new line

--- functions/__update_ps1.sh
+++ functions/__update_ps1.sh
@@ -1,9 +1,9 @@
 __update_ps1() {
     if [ -n "$(type -t __git_ps1)" ] && [ "$(type -t __git_ps1)" == function ]; then
         if [[ $TERM == *256color* ]]; then
-            PS1="\[\e]0;\u@\h: \w\a\]${PROMPT_CHAIN_COLOR}$(__git_ps1 " (%s)")$INPUT_COLOR"'\$ ';
+            PS1="\[\e]0;\u@\h: \w\a\]${PROMPT_CHAIN_COLOR}$(__git_ps1 " (%s)")\n$INPUT_COLOR"'\$ ';
         else
-            PS1='\[\e]0;\u@\h: \w\a\]'"${PROMPT_CHAIN}$(__git_ps1 " (%s)")"'\$ ';
+            PS1='\[\e]0;\u@\h: \w\a\]'"${PROMPT_CHAIN}$(__git_ps1 " (%s)")\n"'\$ ';
         fi
     else
         if [[ -f /etc/local/git-prompt.sh ]]; then
