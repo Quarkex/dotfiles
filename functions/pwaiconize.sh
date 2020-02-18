2b98ae6a18d940d0c660558bc8763dcfc791e9f1
Manlio Joaquín García González <quarkex@pm.me>
2020-02-18 17:49:23 +0000

    Add 128 to the pwa icons

--- functions/pwaiconize.sh
+++ functions/pwaiconize.sh
@@ -3,7 +3,7 @@ function pwaiconize {(
         file="${filename%%.*}"
         ext="${filename#$file}"
         instruction="convert \"$filename\" "
-        for n in 512 256 384 192 152 144 96 72 48; do
+        for n in 512 256 384 192 152 128 144 96 72 48; do
             instruction="$instruction \( -clone 0 -resize ${n}x${n} -write \"$file-$n${ext,,}\" \) "
         done
         instruction="$instruction -alpha on null: "
